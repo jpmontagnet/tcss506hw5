6612fc04ee73584a4771a6f6e881e4ecb87b2066
JP Montagnet <x-github@jope.net>
2022-05-29 15:02:20 -0700

    Cosmetic cleanups to imported code.

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email  # unused: EqualTo
 from yelp import find_coffee
-from flask_login import current_user, login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user  # unused: current_user
 from models import db, login, UserModel
 
-class loginForm(FlaskForm):
-    email=StringField(label="Enter email", validators=[DataRequired(),Email()])
-    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=16)])
-    submit=SubmitField(label="Login")
+class LoginForm(FlaskForm):
+    email = StringField(label="Enter email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Enter password", validators=[DataRequired(), Length(min=6, max=16)])
+    submit = SubmitField(label="Login")
 
-#passwords={}
-#passwords['lhhung@uw.edu']='qwerty'
 
 app = Flask(__name__)
-app.secret_key="a secret"
+app.secret_key = "a secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/login.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -23,19 +21,19 @@ db.init_app(app)
 login.init_app(app)
 
 def addUser(email, password):
-    #check if email or username exits
-    user=UserModel()
+    # check if email or username exits
+    user = UserModel()
     user.set_password(password)
-    user.email=email
+    user.email = email
     db.session.add(user)
     db.session.commit()
 
 @app.before_first_request
 def create_table():
     db.create_all()
-    user = UserModel.query.filter_by(email = "lhhung@uw.edu").first()
+    user = UserModel.query.filter_by(email="lhhung@uw.edu").first()
     if user is None:
-        addUser("lhhung@uw.edu","qwerty")    
+        addUser("lhhung@uw.edu", "qwerty")
     
 @app.route("/home")
 @login_required
@@ -46,18 +44,18 @@ def findCoffee():
 def redirectToLogin():
     return redirect("/login")
 
-@app.route("/login",methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form=loginForm()
+    form = LoginForm()
     if form.validate_on_submit():
         if request.method == "POST":
-            email=request.form["email"]
-            pw=request.form["password"]
-            user = UserModel.query.filter_by(email = email).first()
-            if user is not None and user.check_password(pw) :
+            email = request.form["email"]
+            pw = request.form["password"]
+            user = UserModel.query.filter_by(email=email).first()
+            if user is not None and user.check_password(pw):
                 login_user(user)
                 return redirect('/home')
-    return render_template("login.html",form=form)
+    return render_template("login.html", form=form)
 
 @app.route('/logout')
 def logout():
@@ -65,4 +63,4 @@ def logout():
     return redirect('/login')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
